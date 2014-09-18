fe6a80252cf6d40326cd0e977df3836edd78b68d
unknown <jps1545@GDD-46.main.ad.rit.edu>
2014-09-18 09:05:22 -0400

    Fish Tank! (Moving player + VERY basic AI)

---
+++ Data Defense/Assets/Scripts/AIController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIController : GameEntity {
+
+    protected Vector3 randomVector;
+
+    protected float travelEndTime;
+    protected float minTravelTime = 1;
+    protected float maxTravelTime = 5;
+
+	protected void Start(){
+	    base.Start();
+        travelEndTime = Time.time;
+	}
+	
+	protected void Update(){
+        if(travelEndTime <= Time.time){
+            randomVector = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f));
+            travelEndTime = Time.time + Random.Range(minTravelTime, maxTravelTime);
+            Debug.Log(randomVector.x + "\t" + randomVector.z);
+            Debug.Log(travelEndTime);
+        }
+
+        movement = randomVector;
+        base.Update();
+	}
+}
---
+++ Data Defense/Assets/Scripts/GameEntity.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class GameEntity : MonoBehaviour {
+
+    public float maxSpeed = 15;
+
+    protected Vector3 movement;
+
+    protected CharacterController charController;
+
+	protected void Start(){
+        charController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	protected void Update(){
+        movement.y = Physics.gravity.y;
+        charController.Move(movement * maxSpeed * Time.deltaTime);
+        movement = Vector3.zero;
+	}
+}
