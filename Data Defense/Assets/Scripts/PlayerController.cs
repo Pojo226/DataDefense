bf3222d9d2ad28678125ea52ebb69aa020738028
James Sundra <pojo226@gmail.com>
2014-10-08 15:01:58 -0400

    Tie-ins for XYBA buttons for all 4 joysticks.

--- Data Defense/Assets/Scripts/PlayerController.cs
+++ Data Defense/Assets/Scripts/PlayerController.cs
@@ -11,16 +11,39 @@ public class PlayerController : GameEntity {
 	
 	// Update is called once per frame
 	protected void Update(){
-        float axis = Input.GetAxis("Horizontal" + playerIndex);
+        float axis = 0;
+        //float axis = Input.GetAxis("Horizontal" + playerIndex);
         if(axis != 0) {
             movement.x = axis;
         }
 
-        axis = Input.GetAxis("Vertical" + playerIndex);
+        //axis = Input.GetAxis("Vertical" + playerIndex);
         if(axis != 0) {
             movement.z = -axis;
         }
         
         base.Update();
+
+        if (Input.GetButtonDown("X_" + playerIndex))
+        {
+            Debug.Log("Player" + playerIndex + ": X");
+        }
+
+        if (Input.GetButtonDown("Y_" + playerIndex))
+        {
+            Debug.Log("Player" + playerIndex + ": Y");
+        }
+
+        if (Input.GetButtonDown("B_" + playerIndex))
+        {
+            Debug.Log("Player" + playerIndex + ": B");
+        }
+
+        if (Input.GetButtonDown("A_" + playerIndex))
+        {
+            Debug.Log("Player" + playerIndex + ": A");
+        }
 	}
+
+
 }
