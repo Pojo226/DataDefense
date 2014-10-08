44dc01264f9ef1a0831c77fa0787f88325bafff6
James Sundra <pojo226@gmail.com>
2014-10-08 14:42:34 -0400

    Added support for joysticks 1-4 (X/Y axis only)

--- Data Defense/Assets/Scripts/PlayerController.cs
+++ Data Defense/Assets/Scripts/PlayerController.cs
@@ -1,1 +1,26 @@
-﻿using UnityEngine;using System.Collections;public class PlayerController : GameEntity {	protected void Start(){        base.Start();	}		// Update is called once per frame	protected void Update(){        float axis = Input.GetAxis("Horizontal");        if(axis != 0) {            movement.x = axis;        }        axis = Input.GetAxis("Vertical");        if(axis != 0) {            movement.z = axis;        }                base.Update();	}}
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : GameEntity {
+
+    public int playerIndex;
+
+	protected void Start(){
+        base.Start();
+	}
+	
+	// Update is called once per frame
+	protected void Update(){
+        float axis = Input.GetAxis("Horizontal" + playerIndex);
+        if(axis != 0) {
+            movement.x = axis;
+        }
+
+        axis = Input.GetAxis("Vertical" + playerIndex);
+        if(axis != 0) {
+            movement.z = -axis;
+        }
+        
+        base.Update();
+	}
+}
