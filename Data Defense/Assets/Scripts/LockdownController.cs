b4097afcf57e0de17d91eaa329143f5e30aecb04
DavidK <davidck2010@yahoo.com>
2014-12-13 14:58:10 -0500

    The lockdown prefab and a script that needs work

---
+++ Data Defense/Assets/Scripts/LockdownController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockdownController : MonoBehaviour {
+
+    // Reference for players
+    FixedValues fixedGO;
+
+    // The player who initiated the lock
+    public int myPlayer;
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, 5.0f);
+        fixedGO = GameObject.Find("Fixed Values").GetComponent<FixedValues>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnDestroy()
+    {
+        fixedGO.players[myPlayer].canLock = true;
+    }
+}
