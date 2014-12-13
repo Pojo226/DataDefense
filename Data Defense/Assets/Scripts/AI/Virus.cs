9199e5e98dc6c0606183739fab4568911b4115ed
James Sundra <jps1545@rit.edu>
2014-12-13 14:36:49 -0500

    Viruses spawn programs now

---
+++ Data Defense/Assets/Scripts/AI/Virus.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Virus : AIController {
+
+	private static float Spawn_Time_Min = 2;
+	private static float Spawn_Time_Max = 5;
+
+	private float spawnTime = 0;
+
+	protected void Start(){
+		CalculateSpawnTime();
+		base.Start();
+	}
+
+	protected void Update(){
+		if(spawnTime <= Time.time){
+			CalculateSpawnTime();
+			AISpawner.SpawnImmediately(FixedValues.Enemy_Types.Program, 1, transform);
+		}
+		base.Update();
+	}
+
+	private void CalculateSpawnTime(){
+		spawnTime = Time.time + Random.Range(Spawn_Time_Min, Spawn_Time_Max);
+		Debug.Log(spawnTime - Time.time);
+	}
+}
